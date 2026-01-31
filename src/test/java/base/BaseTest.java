bc1ac680f9fe9727260b9ac8f1565658dde22c86
u30v94 <u30v94@INGURWN121008.avl01.avlcorp.lan>
2026-01-31 14:01:38 +0530

    test reporter added

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,12 +6,19 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseTest {
 	
 	protected WebDriver driver;
-
+	protected ExtentReports extent;
+	protected ExtentTest test;
+	
 	@BeforeClass
 	public void setup() throws IOException {
 		Properties props = new Properties();
@@ -20,8 +27,17 @@ public class BaseTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(props.getProperty("url"));
+		
+		extent = ExtentManager.getExtent();
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/Output.html");
+		extent.attachReporter(spark);
 	}
 	
+	@AfterSuite
+	public void updateReport() {
+		ExtentManager.getExtent().flush();
+
+	}
 	@AfterClass
 	public void teardown() {
 		driver.quit();
